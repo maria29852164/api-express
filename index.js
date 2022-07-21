bf1f47294d0d2d1bdf4cc1692f17b4e27097cad9
maria de los angeles <mariaramireeez2002@gmail.com>
2022-07-21 09:02:28 -0400

    cors y ajustes en validator

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 import express, {Router} from 'express';
 import {routesApi} from './routes/router'
+import cors from 'cors'
 import {defineMiddlewares} from "./middlewares/kernel.middleware";
 const app =  express()
 
 const port = process.env.PORT || 3001;
 // define routes
+const whileList= ['https://localhost:9000']
+const options = {
+  origin: (origin , callback)=> {
+    if(whileList.includes(origin)){
+      callback(null,true)
+    }else{
+      callback(new Error('Sin acceso'))
+    }
+  }
+}
 
-
-
+app.use(cors(options))
 routesApi(app)
 defineMiddlewares(app)
 
--- schemas/products/product.schema.js
+++ schemas/products/product.schema.js
@@ -3,14 +3,16 @@ import joi, {number, string}  from 'joi'
 const name = joi.string().alphanum().min(3).max(20);
 const price = joi.number().integer().min(10);
 const id= joi.string().uuid()
+const image = joi.string().uri()
 
 export const createProductSchema =  joi.object({
     name: name.required(),
-    price: price.required()
+    price: price.required(),
+    image: image.required()
   }
 )
 export const updateProductSchema =  joi.object({
-    name, price
+    name, price, image
   }
 )
 export const getProductSchema = joi.object({id})
